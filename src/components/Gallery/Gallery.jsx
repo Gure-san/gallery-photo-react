e4d528597d314dc8f9ea3e9969f113ce7e254113
Gure-san <azrarafani@gmail.com>
2022-09-08 12:54:07 +0700

    mulai membuat components

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,23 @@
+export function Gallery() {
+  return (
+    <div 
+    className="">
+      {/* Col 1 */}
+      <div>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+      </div>
+
+      {/* Col 2 */}
+      <div>
+
+      </div>
+
+      {/* Col 3 */}
+      <div>
+
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,8 @@
+export function Title() {
+  return (
+    <h1
+    className="font-bold text-[24px] text-dark my-8">
+      Gallery App with React
+    </h1>
+  )
+}
